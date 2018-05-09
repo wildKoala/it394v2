88052badd8d55fa393b9f575aa1a7a7787920730
Gordon <x97275@usmawkddth17602.eecs.usma.edu>
2018-05-09 09:22:06 -0400

    Adds Heroku files for deployment

--- it394project/settings.py
+++ it394project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +130,10 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#added from heroku lsn 5 ICE
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'
+)
+STATICFILES_STORAGE
+=
+'whitenoise.storage.CompressedManifestStaticFilesStorage'
